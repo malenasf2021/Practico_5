c35e88b3d3a8f020f55d1612ed74c8a9e5a23734
Malena Sánchez <93945166+malenasf2021@users.noreply.github.com>
2025-09-08 10:03:57 -0300

    Update Guitarra.java

--- src/main/java/Ejercicio8y9/Guitarra.java
+++ src/main/java/Ejercicio8y9/Guitarra.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Ejercicio8y9;
 
-/**
- *
- * @author Malena
- */
 public class Guitarra extends InstrumentoMusical {
 
     @Override
